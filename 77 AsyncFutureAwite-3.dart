eb2dc93cb1414c23674678d1910640548c4d301a
DeveloperSanju <sanjuwings04@gmail.com>
2020-10-20 11:50:55 +0530

    asyncAwaitFuture

--- 77 AsyncFutureAwite-3.dart
+++ 77 AsyncFutureAwite-3.dart
@@ -37,3 +37,30 @@ void countSeconds(int s) {
     Future.delayed(Duration(seconds: i), () => print(i));
   }
 }
+
+________________________________________________________________
+
+
+import 'dart:async';
+
+class Employee {
+  int id;
+  String firstName;
+  String lastName;
+  
+  Employee(this.id, this.firstName, this.lastName);
+}
+
+void main() async {
+  print("Hello everyone...🙂....");
+  var a = await getEmployee(04);
+  print("good morning ${a.firstName} ${a.lastName} your id ${a.id}  verified");
+}
+
+Future<Employee> getEmployee(int id) async {
+    
+  await Future<Employee>.delayed(const Duration(seconds: 2));
+  
+  var e = new Employee(id, "sanjeev", "Vivekanandan");
+  return e;
+}
