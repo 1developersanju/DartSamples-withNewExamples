c960de4ae86d99107f46a918f00316b8db80cb0a
DeveloperSanju <sanjuwings04@gmail.com>
2020-10-21 10:01:00 +0530

    Dart Exceptions

--- 10 Exceptions-3.dart
+++ 10 Exceptions-3.dart
@@ -51,3 +51,19 @@ main() {
       print('Finally block executed'); 
    } 
 }
+_______________________________________________________________________________________
+
+
+main() { 
+   try { 
+      test_age(-2); 
+   } 
+   catch(e) { 
+      print('Age cannot be negative'); 
+   } 
+}  
+void test_age(int age) { 
+   if(age<0) { 
+      throw new FormatException(); 
+   } 
+}
