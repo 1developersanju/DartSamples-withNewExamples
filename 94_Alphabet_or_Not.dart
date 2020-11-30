de910bab8eaccec39741557c21ded22a26426e20
DeveloperSanju <sanjuwings04@gmail.com>
2020-11-30 17:35:54 +0530

    To check the character is an alphabet or not an alphabet

---
+++ 94_Alphabet_or_Not.dart
@@ -0,0 +1,16 @@
+var cha='b';
+
+void main() {
+  
+  var s = cha.codeUnitAt(0);
+if(
+   ((s>64)&&(s<91)) ||
+  ((s>96)&&(s<123))
+                        
+)
+  
+  print('The charecter $cha is an Alphabet');
+else
+  print('The charecter $cha is not an Alphabet');
+ 
+}
